f19e9e0ffaf4ee1798f496749bc84b0d4850d944
gurleen kaur mago <mago.gurleen@gmail.com>
2019-11-15 00:09:00 +1100

    commit test

--- test/test.js
+++ test/test.js
@@ -6,12 +6,14 @@ describe('GET /', function() {
  //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('Hello World', done);
  });
-
- ch.exec('git rev-parse HEAD', function(err, stdout) {
+});
+describe('GET /', function() { 
+ch.exec('git rev-parse HEAD', function(err, stdout) {
 
 
  it('respond with description, version and last commit hash', function(done)  {
      request(app).get('/status').expect('{"myapplication":[{"version":"1.0","description":"pre-interview technical test","lastcommitsha":stdout}]}',done);
  })
+
 });
 });
