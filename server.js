4fa634cedf5d4e5e82734c53167bfef751f51ff4
gurleen kaur mago <mago.gurleen@gmail.com>
2019-11-14 19:24:42 +1100

    Release 1

---
+++ server.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+//const http = require('http');
+const express = require('express');
+//const itemsRouter = require('./routes/items');
+const app = express();
+app.use(express.json()); //{id:11 name:'nitin'}
+
+const fs = require('fs');
+
+//app.use('/status', itemsRouter);
+
+
+
+app.get('/', function(req, res) {
+
+    res.send('Hello World');
+});
+
+app.get('/status',function(req,res){
+
+
+    let rawdata = fs.readFileSync('./config/metadata.json');
+    let metadata = JSON.parse(rawdata);
+    //console.log(metadata);
+
+    res.json(
+        {
+        "myapplication": [
+                {
+            "version": metadata.version,
+            "description": metadata.description,
+            "lastcommitsha": "abc57858585"
+            }
+        ]}
+        );
+})
+
+const port = 3000;
+app.listen(port,function(){
+     console.log('API is running at port ',port);
+    })
