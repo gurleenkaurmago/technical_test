b8864f814a574a5c1ad76612abe5bd7c82d54643
gurleen kaur mago <mago.gurleen@gmail.com>
2019-11-14 23:30:36 +1100

    test version1

--- test/test.js
+++ test/test.js
@@ -5,4 +5,8 @@ describe('GET /', function() {
  //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('Hello World', done);
  });
+
+ it('respond with description, version and last commit hash', function(done)  {
+     request(app).get('/status').expect('{"myapplication":[{"version":"1.0","description":"pre-interview technical test","lastcommitsha":"24627fd0a09f42adaca09ffda9e26a531052f6a3\n"}]}',done);
+ })
 });
